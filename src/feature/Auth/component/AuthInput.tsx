5afe124942ce03e26020ba33b5fb7c7218df20ab
Hugo Martinez <hugo.83300@gmail.com>
2021-10-26 23:29:45 +0200

    feat(signup): first part of input control

--- src/feature/Auth/component/AuthInput.tsx
+++ src/feature/Auth/component/AuthInput.tsx
@@ -1,16 +1,29 @@
 import { IInputProps, Input } from 'native-base';
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
+import { InputTypes } from '../../../constants/inputs';
 
 type AuthInputProps = {
   placeholder: string;
+  type: InputTypes;
+  onChangeCustom: (type: InputTypes, value: string) => void;
 };
 
 export const AuthInput: FC<AuthInputProps & IInputProps> = ({
   placeholder,
+  type,
+  onChangeCustom,
   ...props
 }) => {
+  const onChangeText = useCallback(
+    (value: string) => {
+      onChangeCustom(type, value);
+    },
+    [onChangeCustom, type]
+  );
+
   return (
     <Input
+      onChangeText={onChangeText}
       size="2xl"
       bgColor="secondary.light"
       color="primary.regular"
--- src/feature/Auth/screens/SignUp.tsx
+++ src/feature/Auth/screens/SignUp.tsx
@@ -1,19 +1,46 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Box, Button, Text, FormControl } from 'native-base';
-import React, { useState, FC } from 'react';
+import React, { useCallback, FC } from 'react';
+import { InputTypes } from '../../../constants/inputs';
+import { validatePassword } from '../../../lib/passwordValidator';
 import { RootStackParamList } from '../../../types/navigation';
+import { validateEmail } from '../../../utils/validateEmail';
 import { AuthInput } from '../component/AuthInput';
 import { AuthLayout } from '../component/AuthLayout';
-import { validateEmail } from '../utils/validateEmail';
 
 type SignUProps = NativeStackScreenProps<RootStackParamList, 'SignIn'>;
 
 export const SignUp: FC<SignUProps> = ({ navigation }) => {
-  const [email, setEmail] = useState<string>('');
-  const [, setPassword] = useState<string>('');
-  const [, setConfirmPassword] = useState<string>('');
+  const [formData, setData] = React.useState(new Map<InputTypes, string>());
+  const [errors, setErrors] = React.useState(new Map<InputTypes, boolean>());
+  const onChange = useCallback(
+    (type: InputTypes, value: string) => setData(formData.set(type, value)),
+    [formData]
+  );
 
-  const isValidEmail = email ? validateEmail(email) : true;
+  const validate = useCallback(() => {
+    const changeableErrors = Object.assign(errors, {});
+    formData.forEach((value, key) => {
+      if (key === InputTypes.EMAIL) {
+        validateEmail(value)
+          ? changeableErrors.delete(InputTypes.EMAIL)
+          : changeableErrors.set(InputTypes.EMAIL, true);
+      }
+
+      if (key === InputTypes.PASSWORD) {
+        validatePassword(value)
+          ? changeableErrors.delete(InputTypes.PASSWORD)
+          : changeableErrors.set(InputTypes.PASSWORD, true);
+      }
+
+      if (key === InputTypes.CONFIRM_PASSWORD) {
+        formData.get(InputTypes.PASSWORD) === value
+          ? changeableErrors.delete(InputTypes.CONFIRM_PASSWORD)
+          : changeableErrors.set(InputTypes.CONFIRM_PASSWORD, true);
+      }
+    });
+    setErrors(changeableErrors);
+  }, [errors, formData]);
 
   return (
     <AuthLayout text="Création de compte">
@@ -26,8 +53,16 @@ export const SignUp: FC<SignUProps> = ({ navigation }) => {
         testID="login-container"
         alignItems="center"
       >
-        <FormControl width="250px" isInvalid={!isValidEmail} mb={6}>
-          <AuthInput onChangeText={setEmail} placeholder="Email" />
+        <FormControl
+          width="250px"
+          isInvalid={errors.has(InputTypes.EMAIL)}
+          mb={6}
+        >
+          <AuthInput
+            type={InputTypes.EMAIL}
+            onChangeCustom={onChange}
+            placeholder="Email"
+          />
           <FormControl.ErrorMessage position="absolute" bottom={-20} left="0">
             <Text fontSize="xs" color="error.regular">
               Please use a valid email
@@ -35,13 +70,54 @@ export const SignUp: FC<SignUProps> = ({ navigation }) => {
           </FormControl.ErrorMessage>
         </FormControl>
 
-        <AuthInput onChangeText={setPassword} placeholder="Password" mb={6} />
+        <FormControl
+          width="250px"
+          isInvalid={errors.has(InputTypes.PASSWORD)}
+          mb={6}
+        >
+          <AuthInput
+            type={InputTypes.PASSWORD}
+            onChangeCustom={onChange}
+            placeholder="Password"
+          />
+          <FormControl.ErrorMessage position="absolute" bottom={-20} left="0">
+            <Text fontSize="xs" color="error.regular">
+              Please use a password with at least 8 characters
+            </Text>
+          </FormControl.ErrorMessage>
+        </FormControl>
+
+        <FormControl
+          width="250px"
+          isInvalid={errors.has(InputTypes.CONFIRM_PASSWORD)}
+          mb={6}
+        >
+          <AuthInput
+            type={InputTypes.CONFIRM_PASSWORD}
+            onChangeCustom={onChange}
+            placeholder="Password confirmation"
+          />
+          <FormControl.ErrorMessage position="absolute" bottom={-20} left="0">
+            <Text fontSize="xs" color="error.regular">
+              Password and its confirmation should be identical
+            </Text>
+          </FormControl.ErrorMessage>
+        </FormControl>
         <AuthInput
-          onChangeText={setConfirmPassword}
+          type={InputTypes.CONFIRM_PASSWORD}
+          onChangeCustom={onChange}
           placeholder="Confirm pasword"
           mb={6}
         />
-        <Button rounded={0} px={6} py={3} maxWidth="100%" width="250px" my={6}>
+        <Button
+          onPress={validate}
+          rounded={0}
+          px={6}
+          py={3}
+          maxWidth="100%"
+          width="250px"
+          my={6}
+        >
           Créer mon compte
         </Button>
         <Button
