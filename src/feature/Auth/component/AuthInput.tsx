01cde1c33ff8dfc5be4a4c0692150daae10a8717
Hugo Martinez <hugo.83300@gmail.com>
2021-11-02 23:07:54 +0100

    feat(singup): use form lib

--- src/feature/Auth/component/AuthInput.tsx
+++ src/feature/Auth/component/AuthInput.tsx
@@ -1,34 +1,13 @@
 import { IInputProps, Input } from 'native-base';
-import React, { FC, useCallback } from 'react';
-import { InputTypes } from '../../../constants/inputs';
+import React, { FC } from 'react';
 
-type AuthInputProps = {
-  placeholder: string;
-  type: InputTypes;
-  onChangeCustom: (type: InputTypes, value: string) => void;
-};
-
-export const AuthInput: FC<AuthInputProps & IInputProps> = ({
-  placeholder,
-  type,
-  onChangeCustom,
-  ...props
-}) => {
-  const onChangeText = useCallback(
-    (value: string) => {
-      onChangeCustom(type, value);
-    },
-    [onChangeCustom, type]
-  );
-
+export const AuthInput: FC<IInputProps> = ({ ...props }) => {
   return (
     <Input
-      onChangeText={onChangeText}
       size="2xl"
       bgColor="secondary.light"
       color="primary.regular"
       variant="filled"
-      placeholder={placeholder}
       rounded="0"
       width="250px"
       maxWidth="100%"
--- src/feature/Auth/screens/SignUp.tsx
+++ src/feature/Auth/screens/SignUp.tsx
@@ -1,46 +1,25 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Box, Button, Text, FormControl } from 'native-base';
-import React, { useCallback, FC } from 'react';
-import { InputTypes } from '../../../constants/inputs';
-import { validatePassword } from '../../../lib/passwordValidator';
+import { useForm, Controller } from 'react-hook-form';
+import { Box, Button, Text } from 'native-base';
+import React, { FC } from 'react';
 import { RootStackParamList } from '../../../types/navigation';
-import { validateEmail } from '../../../utils/validateEmail';
 import { AuthInput } from '../component/AuthInput';
 import { AuthLayout } from '../component/AuthLayout';
 
 type SignUProps = NativeStackScreenProps<RootStackParamList, 'SignIn'>;
+type FormData = {
+  email: string;
+  password: string;
+  comfirmPassword: string;
+};
 
 export const SignUp: FC<SignUProps> = ({ navigation }) => {
-  const [formData, setData] = React.useState(new Map<InputTypes, string>());
-  const [errors, setErrors] = React.useState(new Map<InputTypes, boolean>());
-  const onChange = useCallback(
-    (type: InputTypes, value: string) => setData(formData.set(type, value)),
-    [formData]
-  );
-
-  const validate = useCallback(() => {
-    const changeableErrors = Object.assign(errors, {});
-    formData.forEach((value, key) => {
-      if (key === InputTypes.EMAIL) {
-        validateEmail(value)
-          ? changeableErrors.delete(InputTypes.EMAIL)
-          : changeableErrors.set(InputTypes.EMAIL, true);
-      }
-
-      if (key === InputTypes.PASSWORD) {
-        validatePassword(value)
-          ? changeableErrors.delete(InputTypes.PASSWORD)
-          : changeableErrors.set(InputTypes.PASSWORD, true);
-      }
-
-      if (key === InputTypes.CONFIRM_PASSWORD) {
-        formData.get(InputTypes.PASSWORD) === value
-          ? changeableErrors.delete(InputTypes.CONFIRM_PASSWORD)
-          : changeableErrors.set(InputTypes.CONFIRM_PASSWORD, true);
-      }
-    });
-    setErrors(changeableErrors);
-  }, [errors, formData]);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onSubmit = (data: FormData) => console.log(data);
 
   return (
     <AuthLayout text="Création de compte">
@@ -53,73 +32,19 @@ export const SignUp: FC<SignUProps> = ({ navigation }) => {
         testID="login-container"
         alignItems="center"
       >
-        <FormControl
-          width="250px"
-          isInvalid={errors.has(InputTypes.EMAIL)}
-          mb={6}
-        >
-          <AuthInput
-            type={InputTypes.EMAIL}
-            onChangeCustom={onChange}
-            placeholder="Email"
-          />
-          <FormControl.ErrorMessage position="absolute" bottom={-20} left="0">
-            <Text fontSize="xs" color="error.regular">
-              Please use a valid email
-            </Text>
-          </FormControl.ErrorMessage>
-        </FormControl>
-
-        <FormControl
-          width="250px"
-          isInvalid={errors.has(InputTypes.PASSWORD)}
-          mb={6}
-        >
-          <AuthInput
-            type={InputTypes.PASSWORD}
-            onChangeCustom={onChange}
-            placeholder="Password"
-          />
-          <FormControl.ErrorMessage position="absolute" bottom={-20} left="0">
-            <Text fontSize="xs" color="error.regular">
-              Please use a password with at least 8 characters
-            </Text>
-          </FormControl.ErrorMessage>
-        </FormControl>
-
-        <FormControl
-          width="250px"
-          isInvalid={errors.has(InputTypes.CONFIRM_PASSWORD)}
-          mb={6}
-        >
-          <AuthInput
-            type={InputTypes.CONFIRM_PASSWORD}
-            onChangeCustom={onChange}
-            placeholder="Password confirmation"
-          />
-          <FormControl.ErrorMessage position="absolute" bottom={-20} left="0">
-            <Text fontSize="xs" color="error.regular">
-              Password and its confirmation should be identical
-            </Text>
-          </FormControl.ErrorMessage>
-        </FormControl>
-        <AuthInput
-          type={InputTypes.CONFIRM_PASSWORD}
-          onChangeCustom={onChange}
-          placeholder="Confirm pasword"
-          mb={6}
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <AuthInput onBlur={onBlur} onChangeText={onChange} value={value} />
+          )}
+          name="email"
+          defaultValue=""
         />
-        <Button
-          onPress={validate}
-          rounded={0}
-          px={6}
-          py={3}
-          maxWidth="100%"
-          width="250px"
-          my={6}
-        >
-          Créer mon compte
-        </Button>
+        {errors.email && <Text>This is required.</Text>}
+        <Button onPress={handleSubmit(onSubmit)}>Créer mon compte</Button>
         <Button
           rounded={0}
           py={3}
